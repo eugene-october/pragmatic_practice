58071175851e924253f9a7cd9400a36fea9a9f07
Eugene Mishchenko <evgenjion@gmail.com>
2026-08-08 09:55:27 +0200

    feat: add simple formatter

--- chapter3/address-book/Program.cs
+++ chapter3/address-book/Program.cs
@@ -1,7 +1,9 @@
-﻿using Google.Protobuf;
+﻿using System.Text.Json;
+using Google.Protobuf;
 
 Console.WriteLine("Hello there!");
 address_book.Entrypoint.RunAddressBookUserStory();
+address_book.Entrypoint.RunTrivialBinaryWriter();
 
 namespace address_book
 {
@@ -15,6 +17,23 @@ namespace address_book
         public static AddressBook Decode(byte[] binaryRepresentation)
         {
             return AddressBook.Parser.ParseFrom(binaryRepresentation);
+        }
+    }
+
+    public class FormatterSimple
+    {
+        public static byte[] Encode(AddressBook obj)
+        {
+            byte[] byteArray = JsonSerializer.SerializeToUtf8Bytes(obj);
+
+            return byteArray;
+        }
+
+        public static AddressBook Decode(byte[] binaryRepresentation)
+        {
+            AddressBook? decoded = JsonSerializer.Deserialize<AddressBook>(binaryRepresentation);
+
+            return decoded!;
         }
     }
 
@@ -39,16 +58,19 @@ namespace address_book
 
         public static void RunTrivialBinaryWriter()
         {
-            string filePath = "data.bin";
-
-            // Create and write to the binary file
-            using (BinaryWriter writer = new BinaryWriter(File.Open(filePath, FileMode.Create)))
+            AddressBook ab = new AddressBook
             {
-                writer.Write(42);                  // Writes a 4-byte integer
-                writer.Write(3.14159);             // Writes an 8-byte double
-                writer.Write(true);                // Writes a 1-byte boolean
-                writer.Write("Hello World");       // Writes a length-prefixed string
-            }
+                Name = "Jack",
+                Address = "Black pearl",
+                Phone = "23482342304",
+                Age = 38
+            };
+
+            byte[] encoded = FormatterSimple.Encode(ab);
+            Console.WriteLine($"encoded: {encoded}, length: {encoded.Count()}");
+
+            AddressBook decoded = FormatterSimple.Decode(encoded);
+            Console.WriteLine($"decoded: {decoded}");
         }
     }
 }
